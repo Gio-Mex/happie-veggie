fe9fe98d0838331f3dbc3ba1881cef6e55b41a1d
Giorgio Messore <giorgio.messore@live.it>
2024-06-07 23:49:45 +0200

    vite.congig.js updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
+import envCompatible from "vite-plugin-env-compatible";
 import { defineConfig } from "vite";
 
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    envCompatible(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
