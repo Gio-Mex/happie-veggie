047a0aebce3c8dc0f96dc866a7187db74686ec89
Giorgio Messore <giorgio.messore@live.it>
2024-06-08 11:43:00 +0200

    last updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
-import envCompatible from "vite-plugin-env-compatible";
 import { defineConfig } from "vite";
 
 
 export default defineConfig({
   plugins: [
     react(),
-    envCompatible(),
   ],
   resolve: {
     alias: {
